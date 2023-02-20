aac1f10a00899b51375ae3b19deaa05d225464e3
Ajay Sharma <ajshrmaofficial@gmail.com>
2023-02-20 19:07:36 +0530

    data retrieval logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,49 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './css/App.css'
+import axios from 'axios'
+
+const server = axios.create({
+  baseURL: 'http://thepwnexperts.com:3001/'
+})
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([])
+  const [cart, setCart] = useState([])
+
+  const addToCart = (id)=> {
+    setCart(prev=>{
+      if(prev.includes(id)) return prev
+      else return [...prev, id]
+    })
+  }
+
+  const showCart = ()=> {
+    server.post('/cal', {id: cart})
+    .then(res=>console.log(res.data))
+    .catch(err=>console.log(err))
+  }
+
+  useEffect(()=>{
+    const getDataController = new AbortController()
+    server.get('/', {signal: getDataController.signal})
+    .then(res=>{
+      res.data?.test.map(item=>{
+        setData(prev=>[...prev, item])  
+      })
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+
+    return ()=> getDataController.abort()
+  }, [])
 
   return (
     <div>
-      <h2>Lorem, ipsum dolor.</h2>
-      <h2>Dolores, omnis praesentium.</h2>
-      <h2>Excepturi, nostrum quas.</h2>
-      <h2>Qui, quas magnam.</h2>
+      {data.map(item=>{
+        return <div key={item._id}>{item.name} <button onClick={()=>addToCart(item.id)}>Add to Cart</button></div>
+      })}
+      <button onClick={showCart}>Show Cart</button>
     </div>
   )
 }
